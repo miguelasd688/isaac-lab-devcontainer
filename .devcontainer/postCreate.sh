b6a0f23684c48b41fd2964bbdf7bdffec0f68136
miguelasd688 <miguel_ayuso@outlook.com>
2025-10-04 20:54:35 +0000

    ros2 installation

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+
 # here you move to your custom asset and install it so you can start working with it directly. 
 #cd extrange_robot
 #python -m pip install -e source/extrange_robot/
